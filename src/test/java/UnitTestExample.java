03b261d08002053ff07c220a2e48c3757ecf6bad
Håkon Sætre Strøm <hakon.satre.strom@nmbu.no>
2022-10-24 23:02:13 +0200

    Sending up failed test to see github Action work

--- src/test/java/UnitTestExample.java
+++ src/test/java/UnitTestExample.java
@@ -2,27 +2,24 @@ import org.LeapYear.LeapYear;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assumptions;
+import org.junit.jupiter.api.Assertions;
 
 class LeapYearTests {
     @Test
     @DisplayName("Check if 2000 is leap year")
     void checkIf2000IsLeapYear() {
         LeapYear leapYear = new LeapYear();
-        Assumptions.assumeTrue(leapYear.isLeapYear(2000));
+        Assertions.assertTrue(leapYear.isLeapYear(2001));
+
     }
-    @Test
-    @DisplayName("Check if 2001 is not a leap year")
-    void checkIf2001IsNotLeapYear() {
-        LeapYear leapYear = new LeapYear();
-        Assumptions.assumeFalse(leapYear.isLeapYear(2001));
-    }
+
     @Test
     @DisplayName("Check if all years from year 0 until 2000 dividable with 4 but not 100 is leap year")
     void checkAllYearsDividableWithFourButNot100() {
         LeapYear leapYear = new LeapYear();
         for (int Year = 0;  Year <= 2000 ; Year += 4) {
             if (Year % 100 != 0){
-                Assumptions.assumeTrue(leapYear.isLeapYear(Year));
+                Assertions.assertTrue(leapYear.isLeapYear(Year));
             }
         }
     }
@@ -32,7 +29,7 @@ class LeapYearTests {
         LeapYear leapYear = new LeapYear();
         for (int Year = 0; Year <= 2000 ; Year++){
             if (Year % 400 == 0) {
-                Assumptions.assumeTrue(leapYear.isLeapYear(Year));
+                Assertions.assertTrue(leapYear.isLeapYear(Year));
             }
         }
     }
@@ -42,7 +39,7 @@ class LeapYearTests {
         LeapYear leapYear = new LeapYear();
         for (int Year = 0; Year <= 2000; Year++){
             if(Year % 4 != 0) {
-                Assumptions.assumeFalse(leapYear.isLeapYear(Year));
+                Assertions.assertFalse(leapYear.isLeapYear(Year));
             }
         }
     }
@@ -53,7 +50,7 @@ class LeapYearTests {
         LeapYear leapYear = new LeapYear();
         for (int Year = 0; Year <= 2000; Year++){
             if (Year % 100 == 0 && Year % 400 != 0){
-                Assumptions.assumeFalse(leapYear.isLeapYear(Year));
+                Assertions.assertFalse(leapYear.isLeapYear(Year));
             }
         }
     }
@@ -61,24 +58,24 @@ class LeapYearTests {
     @DisplayName("See if 1700 was not a Leap year")
     void check1700NotLeapYear() {
         LeapYear leapYear = new LeapYear();
-        Assumptions.assumeFalse(leapYear.isLeapYear(1700));
+        Assertions.assertFalse(leapYear.isLeapYear(1700));
     }
     @Test
     @DisplayName("See if 1800 was not a Leap year")
     void check1800NotLeapYear() {
         LeapYear leapYear = new LeapYear();
-        Assumptions.assumeFalse(leapYear.isLeapYear(1800));
+        Assertions.assertFalse(leapYear.isLeapYear(1800));
     }
     @Test
     @DisplayName("See if 1900 was not a Leap year")
     void check1900NotLeapYear() {
         LeapYear leapYear = new LeapYear();
-        Assumptions.assumeFalse(leapYear.isLeapYear(1900));
+        Assertions.assertFalse(leapYear.isLeapYear(1900));
     }
     @Test
     @DisplayName("See if 2100 is not going to be Leap year")
     void check2100NotLeapYear() {
         LeapYear leapYear = new LeapYear();
-        Assumptions.assumeFalse(leapYear.isLeapYear(2100));
+        Assertions.assertFalse(leapYear.isLeapYear(2100));
     }
 }
